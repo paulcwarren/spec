314d97cf71cbf7cbf0d3fe4283bc7d9e1f898e50
Paul Warren <paul.warren@emc.com>
2017-08-15 16:37:15 -0700

    Add support for grpc client connection shim
    [#149934726](https://www.pivotaltracker.com/story/show/149934726)
    
    Signed-off-by: Kai Xiang <kxiang@pivotal.io>

--- csishim/csi.go
+++ csishim/csi.go
@@ -3,10 +3,11 @@ package csishim
 import (
 	csi "github.com/paulcwarren/spec"
 	"google.golang.org/grpc"
+	"code.cloudfoundry.org/goshims/grpcshim"
 )
 
 //go:generate counterfeiter -o csi_fake/fake_csi.go . Csi
 type Csi interface {
-	NewNodeClient(cc *grpc.ClientConn) csi.NodeClient
+	NewNodeClient(cc grpcshim.ClientConn) csi.NodeClient
 	NewControllerClient(cc *grpc.ClientConn) csi.ControllerClient
 }
--- csishim/csishim.go
+++ csishim/csishim.go
@@ -4,12 +4,13 @@ import (
 	"google.golang.org/grpc"
 
 	spec "github.com/paulcwarren/spec"
+	"code.cloudfoundry.org/goshims/grpcshim"
 )
 
 type CsiShim struct{}
 
-func (c *CsiShim) NewNodeClient(conn *grpc.ClientConn) spec.NodeClient {
-	return spec.NewNodeClient(conn)
+func (c *CsiShim) NewNodeClient(conn grpcshim.ClientConn) spec.NodeClient {
+	return spec.NewNodeClient(conn.(*grpcshim.ClientConnShim).ClientConn)
 }
 
 func (c *CsiShim) NewControllerClient(conn *grpc.ClientConn) spec.ControllerClient {
